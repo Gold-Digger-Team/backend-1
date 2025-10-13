7c4fc0b83854d76d101f9ec8cad606f8bb074743
raihansyahyoga <raihansyahyoga@gmail.com>
2025-10-13 20:27:32 +0700

    deletefunction hitungAngsuranFlask

--- services/flaskClient.js
+++ services/flaskClient.js
@@ -8,27 +8,11 @@ const flask = axios.create({
 
 const MARGIN = 0.0925 // 9.25%
 
-async function hitungAngsuranFlask({ gramase, tenor, dp_pct }) {
-  const { data } = await flask.post('/hitung-angsuran', {
-    gramase,
-    tenor,
-    dp_pct
-  })
-
-  if (typeof data?.nominal !== 'number') {
-    throw new Error(`Flask response tidak valid untuk ${gramase}g-${tenor}bln`)
-  }
-
-  return data.nominal
-}
-
 async function hitungSimulasiCilem({ gramase, tenor, dp, is_percentage = true }) {
-
-  console.log("Input:", { gramase, tenor, dp, is_percentage, MARGIN });
+  console.log('Input:', { gramase, tenor, dp, is_percentage, MARGIN })
 
   const harga_emas_harian = 2000000
   const total_harga_emas = harga_emas_harian * gramase
-
 
   if (is_percentage) {
     dp_rp = total_harga_emas * (dp / 100)
@@ -36,24 +20,24 @@ async function hitungSimulasiCilem({ gramase, tenor, dp, is_percentage = true })
     dp_rp = dp
   }
 
-  console.log("Calculated dp_rp:", dp_rp);
-
+  console.log('Calculated dp_rp:', dp_rp)
 
   const margin_bulanan = MARGIN / 12
   const pokokPembiayaan = total_harga_emas - dp_rp // pokok pinjaman
 
-  console.log("pokokPembiayaan:", pokokPembiayaan, "margin_bulanan:", margin_bulanan);
+  console.log('pokokPembiayaan:', pokokPembiayaan, 'margin_bulanan:', margin_bulanan)
 
-  const faktor = Math.pow(1 + margin_bulanan, tenor);
-  const nominal_angsuran = pokokPembiayaan * ((margin_bulanan * faktor) / (faktor - 1));
-  const total_angsuran = (nominal_angsuran * tenor) + dp_rp;
+  const faktor = Math.pow(1 + margin_bulanan, tenor)
+  const nominal_angsuran = pokokPembiayaan * ((margin_bulanan * faktor) / (faktor - 1))
+  const total_angsuran = nominal_angsuran * tenor + dp_rp
 
   return {
     nominal_angsuran,
     total_angsuran,
     dp_rp
-  };
+  }
 }
 
-module.exports = { hitungAngsuranFlask, hitungSimulasiCilem }
-{}
+module.exports = { hitungSimulasiCilem }
+{
+}
