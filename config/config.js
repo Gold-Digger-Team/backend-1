48ef882c4976a4b7af7f68ab1149cfb000bf8008
raihansyahyoga <raihansyahyoga@gmail.com>
2025-10-14 20:50:43 +0700

    DB_PASS to DB_PASSWORD

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config() // kita akan load file spesifik via npm script
 
 const base = {
   username: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT || 5432),
