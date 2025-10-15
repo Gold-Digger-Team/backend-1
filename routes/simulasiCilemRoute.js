12ef67e34b55fbe3934f51226063efd31a2dad5e
atifahnabilla <atifahnabilla9@gmail.com>
2025-10-15 09:38:09 +0700

    add api get predict result

--- routes/simulasiCilemRoute.js
+++ routes/simulasiCilemRoute.js
@@ -2,5 +2,6 @@ const router = require('express').Router()
 const ctrl = require('../controllers/simulasiCilemController')
 
 router.post('/simulasi-cilem', ctrl.simulasiCilem)
+router.get('/prediksi-emas', ctrl.getPrediksiEmas)
 
 module.exports = router
